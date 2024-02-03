835366224f3bb8372cd075143ea0e2807ff51a62
Takyzz <109335990+Takyzz@users.noreply.github.com>
2024-02-03 22:03:14 +0400

    Add files via upload

---
+++ pages/secondpage.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class SecondPage extends StatelessWidget{
+  @override
+  Widget build(BuildContext context){
+    return Scaffold(
+      backgroundColor: Colors.blue[100],
+      body: Center(
+        child: Text('Second Page'),
+      ));
+  }
+}
